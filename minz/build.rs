17001e8b462ce5ab468dbb30f17d008b7b79bfd0
Kaido Kert <kaidokert@gmail.com>
2026-07-31 07:48:33 -0700

    Delete development cruft

--- minz/build.rs
+++
@@ -1,9 +0,0 @@
-use std::{env, fs, path::Path};
-
-fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let out = Path::new(&out_dir);
-    fs::copy("memory.x", out.join("memory.x")).expect("copy memory.x to OUT_DIR");
-    println!("cargo:rustc-link-search={}", out.display());
-    println!("cargo:rerun-if-changed=memory.x");
-}
--- minz/memory.x
+++
@@ -1,5 +0,0 @@
-MEMORY
-{
-  FLASH : ORIGIN = 0x08000000, LENGTH = 256K  /* L431KC = 256K; the old 64K was the AM32-64K-part-era assumption */
-  RAM   : ORIGIN = 0x20000000, LENGTH = 48K
-}
